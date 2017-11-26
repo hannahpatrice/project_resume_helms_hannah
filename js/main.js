679596439477a8b8016649976903917c9e24a708
Hannah Helms <helmshannahpatrice@gmail.com>
2017-11-26 16:03:01 -0500

    Elongate experience formatting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     $('#fullpage').fullpage();
+
+    window.sr = ScrollReveal();
+        sr.reveal('.one', {delay: 250, duration: 2500});
+        sr.reveal('.two', {delay: 750, duration: 2500});
+        sr.reveal('.three', {delay: 1250, duration: 2500});
 });
